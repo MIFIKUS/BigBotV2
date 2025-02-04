b1581b086b99bbf85d107ea26e765b1f50b7406a
Mifikus <mifikus322@gmail.com>
2025-02-04 17:27:31 +0300

    Фикс, что открывал клановые тп в сборщике, и ломался в перестановке

--- bots/autosell/funcs/calculations/price.py
+++ bots/autosell/funcs/calculations/price.py
@@ -3,6 +3,8 @@ from bots.autosell.config import DIFFERENCE_IN_PRICE, MINIMAL_PRICE_FOR_RED
 
 def calculate_price(old_price: int, new_price: int, is_global: bool) -> int or bool:
     """Возвращает или старую цену, или новую - 1, в зависимости от разницы в процентах"""
+    if old_price == 0:
+        old_price = 1
     difference = abs(new_price - old_price) / old_price * 100
 
     if is_global:
