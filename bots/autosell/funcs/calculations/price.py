11074e9b09c482cebafc29b8f6e53fc8d77df7f1
Mifikus <mifikus322@gmail.com>
2025-02-10 18:59:33 +0300

    VER 2.0.3 Фикс перестановки

--- bots/autosell/funcs/calculations/price.py
+++ bots/autosell/funcs/calculations/price.py
@@ -4,7 +4,7 @@ from bots.autosell.config import DIFFERENCE_IN_PRICE, MINIMAL_PRICE_FOR_RED
 def calculate_price(old_price: int, new_price: int, is_global: bool) -> int or bool:
     """Возвращает или старую цену, или новую - 1, в зависимости от разницы в процентах"""
     if old_price == 0:
-        old_price = 1
+        return new_price - 1
     difference = abs(new_price - old_price) / old_price * 100
 
     if is_global:
